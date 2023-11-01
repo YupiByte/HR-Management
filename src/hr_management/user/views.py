96fc0a8b2331df4a6d4125ac2ea2c0b7cd495f5f
marrero-solis <marrerosolis@gmail.com>
2023-11-01 07:59:19 -0400

    rename home.html to index.html

--- src/hr_management/user/views.py
+++ src/hr_management/user/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, HttpResponse
 from .models import User
+
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    return render(request, "index.html") # change to an HttpResponse and to index.html
 
 def users(request):
     items = User.objects.all()
     return render(request, "users.html", {"users": items}) # return all items in the User object
+                                                        # change to HttpResponse
