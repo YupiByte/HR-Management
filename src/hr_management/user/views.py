a7fa3cba567fa0efd89560e6e50ff98f90ed4db0
marrero-solis <marrerosolis@gmail.com>
2023-11-01 08:10:20 -0400

    remove comment in user/views.py

--- src/hr_management/user/views.py
+++ src/hr_management/user/views.py
@@ -3,9 +3,8 @@ from .models import User
 
 # Create your views here.
 def home(request):
-    return render(request, "index.html") # change to an HttpResponse and to index.html
+    return render(request, "index.html")
 
 def users(request):
     items = User.objects.all()
     return render(request, "users.html", {"users": items}) # return all items in the User object
-                                                        # change to HttpResponse
