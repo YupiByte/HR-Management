f77aae16a744d3c215aa277c7b837b29deff862a
Luis <luisfernandojavier.velazquez@upr.edu>
2023-11-02 15:33:08 -0400

    added logout and formatted document

--- src/hr_management/authentication/views.py
+++ src/hr_management/authentication/views.py
@@ -1,26 +1,42 @@
+#===================================================================
+# This file contains the views for the authentication app
+#
+#   Functions: 
+#       login_user: This function logs in a user
+#       logout_user: This function logs out a user
+#===================================================================
+
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from user.models import User
-# Create your views here.
 
- 
+
+
+# Login user function
 def login_user(request):
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
 
         # Authenticate user
-        user = authenticate(request, username=username, password=password)
-
+        user = authenticate(request, 
+                            username=username, 
+                            password=password)
         # check if user is found using the model User to authenticate
-
-
         if user is not None:
             login(request, user)
-            messages.success(request, "You have successfully logged in")
+            messages.success(request, 
+                            "You have successfully logged in")
             return redirect("user_home")
         else:
             messages.error(request, "Invalid username or password")
             return redirect("login")
     return render(request, "authenticate/login.html", {}) 
+
+
+# Logout user function
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have successfully logged out")
+    return redirect("login")
