6adadbb9f21f5423982a8f3aeb8df439a745aabb
Luis <luisfernandojavier.velazquez@upr.edu>
2023-11-01 22:12:09 -0400

    view for the login

--- src/hr_management/authentication/views.py
+++ src/hr_management/authentication/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
+
+ 
+def login_user(request):
+    '''if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        # Authenticate user
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have successfully logged in")
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid username or password")
+            return redirect("login")'''
+    return render(request, "authenticate/login.html", {}) 
