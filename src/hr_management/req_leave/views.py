a61e2b15a331e60cca1d9af6d7d7e93ac4cbee52
FrancisKRZ <francisco.melendez4@upr.edu>
2023-11-09 10:08:57 -0400

    Request Module working as intended (also using crispyform's more secure features 'GET' & days off processing), needs some polishing (CSS) and various QoL. The employee id should be fetched by simply getting the current logged user's employee_id , the request_id is generated by uuid1 [:8] up to 8 chars. View Requests is pending, and to be alloted within the same page :) -- Some CSS added to Publications. Cleaned up URLs routing.

--- src/hr_management/req_leave/views.py
+++ src/hr_management/req_leave/views.py
@@ -30,6 +30,8 @@ def request_submit(request):
         request_form.save()
         request_form = RequestCreateForm()
 
+        # return redirect(view_request)
+
 
     context = {"request_form": request_form}
     return render(request, "request.html", context)
@@ -40,62 +42,3 @@ def manage_request(request):
 
     ''''''
 
-
-# def submit_leave_request(request):
-
-#     form = LeaveRequestForm(request.POST or None)
-
-#     if form.is_valid():
-#         form.save()
-#         form = LeaveRequestForm()
-
-
-#     context = {
-#         'form': form
-#     }
-
-#     return render(request, "req_leave/submit_leave_request.html", context)
-
-
-# def submit_leave_request(request):
-
-
-
-#     if request.method == 'POST':
-#         form = LeaveRequestForm(request.POST)
-#         if form.is_valid():
-#             leave_request = form.save(commit=False)
-
-#             leave_request.
-
-#             leave_request.request_status = 'Pending'
-#             leave_request.save()
-#             # return redirect('leave_request_success')  # Redirect to a success page
-#     else:
-#         form = LeaveRequestForm()
-    
-#     context = {'form': form}
-#     return render(request, 'submit_leave_request.html', context)
-
-
-
-# def manage_leave_request(request):
-#     # if not request.user.is_manager:  # Check if the user is a manager
-#         # raise Http404  # Handle unauthorized access
-    
-#     leave_requests = Request.objects.all()
-    
-#     context = {'leave_requests': leave_requests}
-#     return render(request, 'req_leave/manage_leave_request.html', context)
-
-
-
-# def leave_request_approval(request):
-#     if request.method == 'POST':
-#         leave_request_id = request.POST.get('leave_request_id')
-#         status = request.POST.get('status')
-#         leave_request = Request.objects.get(id=leave_request_id)
-#         leave_request.status = status
-#         leave_request.save()
-#     leave_requests = Request.objects.filter(status='Pending')
-#     return render(request, 'request_approval.html', {'leave_requests': leave_requests})
